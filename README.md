9d5bbd2986b14bd5a84c7f67a4501a32a37045f6
teamxtZeus <73162884+teamxtZeus@users.noreply.github.com>
2021-02-22 13:47:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aryan-game
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var garden,gardenImage
+
+
+
+
+function preload (){
+gardenImage = loadImage("/Users/vikirtin/Downloads/my own game/images /background.jpg")
+
+}
+function setup(){
+createCanvas(600,600)
+garden=createSprite(300,300)
+//garden.addImage(gardenImage)
+
+}
+function draw (){
+  drawSprites()
+
+  
+}
